a409abe031c0204f27344e07b462d3d81d0cabe3
JTL <jtl@teamclassified.ca>
2019-09-15 01:03:41 +0000

    Fix PATH enviroment on Gentoo Linux

--- envsetup.sh
+++ envsetup.sh
@@ -9,3 +9,20 @@ chrt -b -p 0 $$
 export PATH="$PWD/script/bin:$PATH"
 export BUILD_USERNAME=grapheneos
 export BUILD_HOSTNAME=grapheneos
+
+# Fix building on Gentoo
+if test -f "/etc/gentoo-release"; then
+    echo "[DEBUG] Applying Gentoo fix"
+    set -x
+    unset JAVAC
+
+    if [ -z $OLDPATH ]; then
+        export OLDPATH=$PATH
+    else
+        export PATH=$OLDPATH
+    fi
+
+    export PATH="/usr/lib/python-exec/python2.7:/usr/lib/python-exec/python3.7:$PATH" # Python 2.7 first, then 3.x
+    export EPYTHON=python2.7
+    set +x
+fi
